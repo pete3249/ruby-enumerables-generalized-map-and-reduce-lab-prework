1f7ce453d4d33f1659858ce44a8b2666d2044145
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-02 03:38:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,18 @@
-# Your Code Here
+def map(source_array)
+  n = []
+  source_array.each do |i|
+    n.push yield i
+  end
+  return n
+end 
+
+def reduce(source_array, starting_value = source_array[0])
+  n = starting_value
+  i=1
+  while i < source_array.length
+    n = yield(n, source_array[i])
+    i+=1
+  end
+  return n
+end
+
